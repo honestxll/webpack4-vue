acb68c556e0dd56f2aca951a2509c407d4361ee7
13451883267 <1131328108@qq.com>
2018-06-07 16:44:07 +0800

    source file

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <ul>
+      <li v-for="m in msg">{{ m }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: [ 'hola~', 'hello!', '你好：）' ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.app {
+  color: red;
+  font-size: 80px;
+  padding: 80px;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke: 1px rgba(#21d5df, 0.31);
+  background: -webkit-linear-gradient(right,#fefefe,#cff1c6);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: rgba(0,0,0,0);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+const Vue = require('vue').default
+import App from './App.vue'
+
+new Vue({
+  el: '.app',
+  render: h => h(App)
+})
